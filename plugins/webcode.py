622c29e8fb7b096d875013ae9e3a69589fba08e5
Bossshaz <hwzmd8hnc2@privaterelay.appleid.com>
2026-06-26 01:48:39 +0530

    feat: Add MX Player & VLC video streaming server with HTTP Range support

--- plugins/webcode.py
+++ plugins/webcode.py
@@ -1,13 +1,14 @@
 from aiohttp import web as webserver
+from plugins.stream_server import stream_routes
 
 routes = webserver.RouteTableDef()
 
 async def bot_run():
     _app = webserver.Application(client_max_size=30000000)
     _app.add_routes(routes)
+    _app.add_routes(stream_routes)
     return _app
-  
+
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return webserver.json_response(" Web Supported . . . !  This is a preview of WeB . . .! ! !")
- 
+    return webserver.json_response("Web Supported . . . ! This is a preview of WeB . . . !!!")
